6b7242bce4ca11792081fab816fcf3ad577b6343
Harri <hartsa94@hotmail.fi>
2025-04-01 15:30:36 +0300

    Cors

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv
 import pickle
 
+load_dotenv()
+
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {f"origins": {os.environ.get("ADDRESS")}}})
 
 sa_model = pickle.load(open("sentiment_analysis_model.sav", "rb"))
 
@@ -19,4 +25,12 @@ def analyze():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8100", debug=False)
+    debug_mode = False
+    
+    if os.environ.get("DEBUG").lower() == "true":
+        debug_mode = True
+    else:
+        debug_mode = False
+
+    app.run(host="0.0.0.0", port="8100", debug=debug_mode)
+            
